9f0bbd1190b735da67cd26784c941b109e58a2fa
Aeronaul <aeronaul@proton.me>
2023-06-18 15:41:12 +0530

    Update ApplicationRunner.java

--- src/tictactoe/ApplicationRunner.java
+++ src/tictactoe/ApplicationRunner.java
@@ -1,9 +1,5 @@
 package tictactoe;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 public class ApplicationRunner {
     public static void main(String[] args) {
         new TicTacToe();
